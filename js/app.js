ac5cf8ef7f8d24ae7a601212984bc1f866113268
Jenny Evans <ebbleberryjenny@gmail.com>
2017-07-23 11:27:18 -0400

    Have functional map and places list

---
+++ js/app.js
@@ -0,0 +1,260 @@
+name//place data/model
+  var places = [
+    {
+      name: 'Park Ave Penthouse',
+      address: '123 Sesame Street',
+      location: {lat: 40.7713024, lng: -73.9632393}
+    },
+    {
+      name: 'Chelsea Loft',
+      location: {lat: 40.7444883, lng: -73.9949465}
+    },
+    {
+      name: 'Union Square Open Floor Plan',
+      location: {lat: 40.7347062, lng: -73.9895759}
+    },
+    {
+      name: 'East Village Hip Studio',
+      location: {lat: 40.7281777, lng: -73.984377}
+    },
+    {
+      name: 'TriBeCa Artsy Bachelor Pad',
+      location: {lat: 40.7195264, lng: -74.0089934}
+    },
+    {
+      name: 'Chinatown Homey Space',
+      location: {lat: 40.7180628, lng: -73.9961237}
+    }
+  ];
+
+//styles data/model to go with map
+  var styles = [
+    {
+      featureType: 'water',
+      stylers: [
+        { color: '#19a0d8' }
+      ]
+    },{
+      featureType: 'administrative',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        { color: '#ffffff' },
+        { weight: 6 }
+      ]
+    },{
+      featureType: 'administrative',
+      elementType: 'labels.text.fill',
+      stylers: [
+        { color: '#e85113' }
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [
+        { color: '#efe9e4' },
+        { lightness: -40 }
+      ]
+    },{
+      featureType: 'transit.station',
+      stylers: [
+        { weight: 9 },
+        { hue: '#e85113' }
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'labels.icon',
+      stylers: [
+        { visibility: 'off' }
+      ]
+    },{
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        { lightness: 100 }
+      ]
+    },{
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [
+        { lightness: -100 }
+      ]
+    },{
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [
+        { visibility: 'on' },
+        { color: '#f0e4d3' }
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'geometry.fill',
+      stylers: [
+        { color: '#efe9e4' },
+        { lightness: -25 }
+      ]
+    }
+  ];
+
+  var Place = function(data) {
+    this.name = ko.observable(data.name);
+    this.address = ko.observable(data.address);
+    //this.nicknames = ko.observableArray(data.nicknames);
+    /*this.level = ko.computed(function(){
+      if (this.clickCount() < 10) {
+        return "Newborn";
+      } else if (this.clickCount() < 20) {
+        return "Teen";
+      } else {
+        return "Adult";
+      }
+    }, this);*/
+  };
+
+  //Make currentPlace change when you click on a place name
+
+  var ViewModel = function() {
+    var self = this;
+
+    this.placeList = ko.observableArray([]);
+
+    places.forEach(function(placeItem){
+      self.placeList.push(new Place (placeItem));
+    });
+
+    this.currentPlace = ko.observable(this.placeList()[0]);
+
+    /*this.incrementCounter = function() {
+      self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };*/
+
+    this.pickPlace = function(clickedPlace) {
+      self.currentPlace(clickedPlace);
+    };
+  }
+
+  ko.applyBindings(new ViewModel());
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+var map;
+
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7413549, lng: -73.9980244},
+    zoom: 13,
+    styles: styles,
+    mapTypeControl: false
+  });
+
+  var largeInfowindow = new google.maps.InfoWindow();
+
+  // Style the markers a bit. This will be our listing marker icon.
+  var defaultIcon = makeMarkerIcon('0091ff');
+
+  // Create a "highlighted location" marker color for when the user
+  // mouses over the marker.
+  var highlightedIcon = makeMarkerIcon('FFFF24');
+
+  // The following group uses the places array to create an array of markers on initialize.
+  for (var i = 0; i < places.length; i++) {
+    // Get the position from the location array.
+    var position = places[i].location;
+    var name = places[i].name;
+    // Create a marker per location, and put into markers array.
+    var marker = new google.maps.Marker({
+      position: position,
+      name: name,
+      animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
+      id: i
+    });
+    // Push the marker to our array of markers.
+    markers.push(marker);
+    // Create an onclick event to open the large infowindow at each marker.
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+    // Two event listeners - one for mouseover, one for mouseout,
+    // to change the colors back and forth.
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
+    });
+  }
+
+  document.getElementById('show-listings').addEventListener('click', showListings);
+
+  // This function populates the infowindow when the marker is clicked. We'll only allow
+  // one infowindow which will open at the marker that is clicked, and populate based
+  // on that markers position.
+  function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      // Clear the infowindow content to give the streetview time to load.
+      infowindow.setContent('');
+      infowindow.marker = marker;
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
+      });
+      var streetViewService = new google.maps.StreetViewService();
+      var radius = 50;
+      // In case the status is OK, which means the pano was found, compute the
+      // position of the streetview image, then calculate the heading, then get a
+      // panorama from that and set the options
+      function getStreetView(data, status) {
+        if (status == google.maps.StreetViewStatus.OK) {
+          var nearStreetViewLocation = data.location.latLng;
+          var heading = google.maps.geometry.spherical.computeHeading(
+            nearStreetViewLocation, marker.position);
+            infowindow.setContent('<div>' + marker.name + '</div><div id="pano"></div>');
+            var panoramaOptions = {
+              position: nearStreetViewLocation,
+              pov: {
+                heading: heading,
+                pitch: 30
+              }
+            };
+          var panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), panoramaOptions);
+        } else {
+          infowindow.setContent('<div>' + marker.name + '</div>' +
+            '<div>No Street View Found</div>');
+        }
+      }
+      // Use streetview service to get the closest streetview image within
+      // 50 meters of the markers position
+      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+      // Open the infowindow on the correct marker.
+      infowindow.open(map, marker);
+    }
+  }
+
+  // This function will loop through the markers array and display them all.
+  function showListings() {
+    var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+      bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+  }
+
+  // This function takes in a COLOR, and then creates a new marker
+  // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+  // of 0, 0 and be anchored at 10, 34).
+  function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21,34));
+    return markerImage;
+  }
+};
