2c8b5990be408d86d220878fd461749742180244
Jenny Evans <ebbleberryjenny@gmail.com>
2017-07-28 00:33:07 -0400

    Ajax request ok, need to change foursquare info based on click

--- js/app.js
+++ js/app.js
@@ -2,169 +2,204 @@ var markers = [];
 var map;
 
 //place data/model
-  var places = [
-    {
-      name: 'Park Ave Penthouse',
-      address: '123 Sesame Street',
-      location: {lat: 40.7713024, lng: -73.9632393},
-      marker: markers,
-      id: 0
-    },
-    {
-      name: 'Chelsea Loft',
-      location: {lat: 40.7444883, lng: -73.9949465},
-      marker: markers,
-      id: 1
-    },
-    {
-      name: 'Union Square Open Floor Plan',
-      location: {lat: 40.7347062, lng: -73.9895759},
-      marker: markers,
-      id: 2
-    },
-    {
-      name: 'East Village Hip Studio',
-      location: {lat: 40.7281777, lng: -73.984377},
-      marker: markers,
-      id: 3
-    },
-    {
-      name: 'TriBeCa Artsy Bachelor Pad',
-      location: {lat: 40.7195264, lng: -74.0089934},
-      marker: markers,
-      id: 4
-    },
-    {
-      name: 'Chinatown Homey Space',
-      location: {lat: 40.7180628, lng: -73.9961237},
-      marker: markers,
-      id: 5
-    }
-  ];
+var places = [
+  {
+    name: 'Eataly',
+    address: '200 5th Ave, New York, NY 10010, USA',
+    location: {lat: 40.742164, lng: -73.989894},
+    marker: markers,
+    id: 0,
+    foursquare: '4c5ef77bfff99c74eda954d3'
+  },
+  {
+    name: 'The Coffee Shop',
+    address: '29 Union Square W, New York, NY 10003, USA',
+    location: {lat: 40.736473, lng: -73.991039},
+    marker: markers,
+    id: 1,
+    foursquare: '3fd66200f964a5204ee41ee3'
+  },
+  {
+    name: 'The Strand Bookstore',
+    address: '828 Broadway, New York, NY 10003, USA',
+    location: {lat: 40.733095, lng: -73.990773},
+    marker: markers,
+    id: 2,
+    foursquare: '4a8cc1d4f964a520130f20e3'
+  },
+  {
+    name: 'Pondicheri Restaurant',
+    address: '15 W 27th St, New York, NY 10001, USA',
+    location: {lat: 40.744449, lng: -73.988307},
+    marker: markers,
+    id: 3,
+    foursquare: '578aad61498edec74e47456c'
+  },
+  {
+    name: 'Veselka Restaurant',
+    address: '144 2nd Ave, New York, NY 10003, USA',
+    location: {lat: 40.729026, lng: -73.987121},
+    marker: markers,
+    id: 4,
+    foursquare: '3fd66200f964a520b8ea1ee3'
+  },
+  {
+    name: 'Momofuku Noodle Bar',
+    address: '171 1st Avenue, New York, NY 10003, USA',
+    location: {lat: 40.729237, lng: -73.984506},
+    marker: markers,
+    id: 5,
+    foursquare: '4731be8af964a520244c1fe3'
+  }
+];
 
 //styles data/model to go with map
-  var styles = [
-    {
-      featureType: 'water',
-      stylers: [
-        { color: '#19a0d8' }
-      ]
-    },{
-      featureType: 'administrative',
-      elementType: 'labels.text.stroke',
-      stylers: [
-        { color: '#ffffff' },
-        { weight: 6 }
-      ]
-    },{
-      featureType: 'administrative',
-      elementType: 'labels.text.fill',
-      stylers: [
-        { color: '#e85113' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'geometry.stroke',
-      stylers: [
-        { color: '#efe9e4' },
-        { lightness: -40 }
-      ]
-    },{
-      featureType: 'transit.station',
-      stylers: [
-        { weight: 9 },
-        { hue: '#e85113' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'labels.icon',
-      stylers: [
-        { visibility: 'off' }
-      ]
-    },{
-      featureType: 'water',
-      elementType: 'labels.text.stroke',
-      stylers: [
-        { lightness: 100 }
-      ]
-    },{
-      featureType: 'water',
-      elementType: 'labels.text.fill',
-      stylers: [
-        { lightness: -100 }
-      ]
-    },{
-      featureType: 'poi',
-      elementType: 'geometry',
-      stylers: [
-        { visibility: 'on' },
-        { color: '#f0e4d3' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'geometry.fill',
-      stylers: [
-        { color: '#efe9e4' },
-        { lightness: -25 }
-      ]
-    }
-  ];
-
-  var Place = function(data) {
-    var self = this;
-
-    this.name = ko.observable(data.name);
-    this.address = ko.observable(data.address);
-    this.marker = ko.observable(data.marker[data.id]);
-    this.infoWindow = ko.observable();
+var styles = [
+  {
+    featureType: 'water',
+    stylers: [
+      { color: '#19a0d8' }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { color: '#ffffff' },
+      { weight: 6 }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { color: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -40 }
+    ]
+  },{
+    featureType: 'transit.station',
+    stylers: [
+      { weight: 9 },
+      { hue: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'labels.icon',
+    stylers: [
+      { visibility: 'off' }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { lightness: 100 }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { lightness: -100 }
+    ]
+  },{
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      { visibility: 'on' },
+      { color: '#f0e4d3' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -25 }
+    ]
   }
+];
 
+var Place = function(data) {
+  var self = this;
 
-  var ViewModel = function() {
-    var self = this;
-    self.placeList = ko.observableArray([]);
-    self.query = ko.observable('');
-    self.currentPlace = ko.observable();
+  this.name = ko.observable(data.name);
+  this.address = ko.observable(data.address);
+  this.marker = ko.observable(data.marker[data.id]);
+  this.infoWindow = ko.observable();
+}
 
-    places.forEach(function(placeItem){
-      self.placeList.push(new Place (placeItem));
-    });
+var ViewModel = function() {
+  var self = this;
+  self.placeList = ko.observableArray([]);
+  self.query = ko.observable('');
+  self.currentPlace = ko.observable();
 
-    //Make currentPlace change when you click on a place name
-    self.currentPlace = ko.observable(self.placeList()[0]);
+  places.forEach(function(placeItem){
+    self.placeList.push(new Place (placeItem));
+  });
 
-    self.pickPlace = function(clickedPlace) {
-      self.currentPlace(clickedPlace);
-    };
+  //Make currentPlace change when you click on a place name
+  self.currentPlace = ko.observable(self.placeList()[0]);
 
-    // Click binding
-    self.markerAnimator = function(index) {
-      google.maps.event.trigger(markers[index], 'click');
-    };
+  self.pickPlace = function(clickedPlace) {
+    self.currentPlace(clickedPlace);
+  };
 
-    self.onClick = function() {
-      self.pickPlace();
-      self.markerAnimator();
-    };
+  // Click binding
+  self.markerAnimator = function(index) {
+    google.maps.event.trigger(markers[index], 'click');
+  };
 
-    self.search = function(value) {
-      // remove all the current places, which removes them from the view
-      self.placeList.removeAll();
-      for(var x in places) {
-        if (places[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-          self.placeList.push(places[x]);
-        }
+  self.onClick = function(clickedPlace, a, b) {
+    self.pickPlace(clickedPlace);
+    self.markerAnimator(clickedPlace.id);
+    self.loadData(clickedPlace.id);
+  };
+
+  self.search = function(value) {
+    // remove all the current places, which removes them from the view
+    self.placeList.removeAll();
+    for(var x in places) {
+      if (places[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        self.placeList.push(places[x]);
       }
-    };
+    }
+  };
 
-    self.query.subscribe(self.search);
-  }
+  self.query.subscribe(self.search);
+}
 
-  ko.applyBindings(new ViewModel());
+ko.applyBindings(new ViewModel());
 
+function loadData() {
+  var $foursquareData = $('#foursquare-content');
 
+  var apiURL = 'https://api.foursquare.com/v2/venues/';
+  var foursquareClientID = 'HUDI4LKRSI4RV5LIDRMDAE1MWRBGNYN3R3SQ1CFPQVUBYX4N'
+  var foursquareSecret ='02OHUV51QTN2BPKHUQJB5UZOOIWPZQBIADXB1JIVKQOIGDVC';
+  var foursquareVersion = '20170723';
+  var venueFoursquareID = places[0].foursquare;
 
-// Create a new blank array for all the listing markers.
+  var foursquareURL = apiURL + venueFoursquareID + '?client_id=' + foursquareClientID +  '&client_secret=' + foursquareSecret +'&v=' + foursquareVersion;
+
+  $.ajax({
+    url: foursquareURL,
+    success: function(data) {
+      console.log(data);
+      var info = data.response.venue;
+      console.log(info);
+      $foursquareData.append('<li class="article">'+
+      '<a href="'+ info.shortUrl + '">Click for more info here</a>' +
+        '<p>' + info.description + '<br>' + info.categories[0].name + '<br>' +
+        '<p> The rating is: ' + info.rating + '</p>'+
+        '</li>');
+    }
+  });
+}
+
+loadData();
 
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
@@ -293,4 +328,5 @@ function initMap() {
       new google.maps.Size(21,34));
     return markerImage;
   }
+
 };
